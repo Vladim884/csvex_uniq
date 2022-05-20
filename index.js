09b46e474cd250e15e5c3cf2603e3f324011c0da
vladimir <vladim.volvenko@gmail.com>
2022-05-20 22:21:09 +0300

    not important dhange

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 // app.use(fileUpload({}))
 app.use(corsMiddleware)
